d2ea328af1338f4a6d98e0c430186baa108e3493
mihirbpi <mihir.borkar@gmail.com>
2020-12-05 12:00:25 -0800

    Used built in binary to decimal

--- adv5/adv5-1.py
+++ adv5/adv5-1.py
@@ -3,19 +3,11 @@ from aocd import get_data
 my_list = get_data(day=5).split("\n")
 
 
-def bin_to_dec(string):
-    result = 0
-
-    for i in range(0, len(string)):
-        digit = int(string[len(string)-1-i])
-        result += digit * (2**i)
-    return result
-
 def seat_ID(string):
     row_string = string[0:7].replace("F", "0").replace("B", "1")
     column_string = string[7:10].replace("L", "0").replace("R", "1")
-    row = bin_to_dec(row_string)
-    column = bin_to_dec(column_string)
+    row = int(row_string, 2)
+    column = int(column_string, 2)
     return row * 8 + column
 
 
