dfa756929bff3231919927513191ff0c6df2b153
mihirbpi <mihir.borkar@gmail.com>
2020-12-06 09:58:24 -0800

    Solved Day 6

---
+++ adv6/adv6-1.py
@@ -0,0 +1,19 @@
+from aocd import get_data
+
+my_list = get_data(day=6).split("\n\n")
+
+for i in range (0, len(my_list)):
+    my_list[i] = my_list[i].replace("\n", "")
+
+count = 0
+
+for i in range (0, len(my_list)):
+    string = my_list[i]
+    questions = set()
+
+    for j in range(0, len(string)):
+        questions.add(string[j])
+
+    count += len(questions)
+
+print(count)
---
+++ adv6/adv6-2.py
@@ -0,0 +1,25 @@
+from aocd import get_data
+
+my_list = get_data(day=6).split("\n\n")
+
+for i in range (0, len(my_list)):
+    my_list[i] = my_list[i].split("\n")
+
+count = 0
+
+for i in range (0, len(my_list)):
+    strings = my_list[i]
+    set_list = []
+
+    for j in range(0, len(strings)):
+        string = strings[j]
+        question_set = set()
+
+        for k in range(0, len(string)):
+            question_set.add(string[k])
+
+        set_list.append(question_set)
+
+    count += len(set.intersection(*set_list))
+
+print(count)
