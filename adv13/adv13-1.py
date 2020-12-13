ace1ec9b2b99760c04759709a7ebbf461e8f3b82
mihirbpi <mihir.borkar@gmail.com>
2020-12-13 10:03:14 -0800

    Solved Day 13 Part 1

---
+++ adv13/adv13-1.py
@@ -0,0 +1,9 @@
+from aocd import get_data
+import math
+
+my_list = get_data(day=13).split("\n")
+arrival_time = int(my_list[0])
+bus_IDs = list(map(int, list(filter(lambda x: x != 'x', my_list[1].split(",")))))
+earliest_times = list(map(lambda x: x * (int(math.floor(arrival_time / x)) + 1) - arrival_time, bus_IDs))
+
+print(min(earliest_times) * bus_IDs[earliest_times.index(min(earliest_times))])
