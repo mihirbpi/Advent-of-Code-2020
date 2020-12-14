5ce8ec636c2bab6e664f60290d95dddd7d55da6a
mihirbpi <mihir.borkar@gmail.com>
2020-12-14 09:56:54 -0800

    Solved Day 14 Part 1

---
+++ adv14/adv14-1.py
@@ -0,0 +1,41 @@
+from aocd import get_data
+
+program = get_data(day=14).split("\n")
+mask = ""
+mem = {}
+
+def replacer(string, newstring, index):
+    return string[:index] + newstring + string[index + 1:]
+
+def dec_to_bin(dec_string):
+    result = bin(int(dec_string)).replace("0b", "")
+    return (36 - len(result)) * "0" + result
+
+def bin_to_dec(bin_string):
+    return int(bin_string, 2)
+
+def new_dec_value(dec_string, mask_string):
+    bin_string = dec_to_bin(dec_string)
+    new_bin_string = bin_string
+
+    for i in range(0, len(mask_string)):
+        char = mask_string[i]
+
+        if(char == "0" or char == "1"):
+            new_bin_string = replacer(new_bin_string, char, i)
+
+    return bin_to_dec(new_bin_string)
+
+for i in range(0, len(program)):
+    string = program[i]
+
+    if(string.split(" ")[0] == "mask"):
+        mask = string.split(" ")[2]
+
+    else:
+        dec_string = string.split(" ")[2]
+        new_value = new_dec_value(dec_string, mask)
+        address = int(string.split("mem[")[1].split("]")[0])
+        mem[address] = new_value
+
+print(sum(mem.values()))
