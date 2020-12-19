17fc6168855f36688f4dfb31360576928b5b9b83
mihirbpi <mihir.borkar@gmail.com>
2020-12-19 15:41:40 -0800

    Formatting

--- adv19/adv19-1.py
+++ adv19/adv19-1.py
@@ -14,46 +14,44 @@ for i in range(0, len(rules)):
 
 def valid_strings_set(rule_number):
     rule_code = rules_dict[rule_number]
+    result = set()
 
     if(rule_code[0] == '"'):
-        result = set()
         result.add(rule_code.strip('"'))
         return result
 
-
     elif("|" not in rule_code):
-        result = set()
         r = rule_code.split(" ")
         length = len(r)
         x = list(map(valid_strings_set, r))
 
         for p in itertools.product(*x):
             result.add("".join(p))
-        return result
-
-    else:
-        result = set()
-        r1 = rule_code.split("|")[0].strip(" ").split(" ")
-        length1 = len(r1)
-        x1 = list(map(valid_strings_set, r1))
-
-        for p1 in itertools.product(*x1):
-            result.add("".join(p1))
-
-        r2 = rule_code.split("|")[1].strip(" ").split(" ")
-        length2 = len(r2)
-        x2 = list(map(valid_strings_set, r2))
-
-        for p2 in itertools.product(*x2):
-            result.add("".join(p2))
 
         return result
+
+    r1 = rule_code.split("|")[0].strip(" ").split(" ")
+    length1 = len(r1)
+    x1 = list(map(valid_strings_set, r1))
+
+    for p1 in itertools.product(*x1):
+        result.add("".join(p1))
+
+    r2 = rule_code.split("|")[1].strip(" ").split(" ")
+    length2 = len(r2)
+    x2 = list(map(valid_strings_set, r2))
+
+    for p2 in itertools.product(*x2):
+        result.add("".join(p2))
+
+    return result
 
 valid_strings = valid_strings_set("0")
 
 count = 0
 
 for i in range(0, len(strings)):
+
     if(strings[i] in valid_strings):
         count +=1
 
