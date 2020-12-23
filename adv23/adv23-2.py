cee3ef81c0f43fa02b0d098820ac40e6b242ded8
mihirbpi <mihir.borkar@gmail.com>
2020-12-23 15:06:25 -0800

    Some speedups for Part 2. Takes 2 min to run if no other processes are running.

--- adv23/adv23-2.py
+++ adv23/adv23-2.py
@@ -24,6 +24,7 @@ class Node(object):
         self.next.prev = self.prev
         self.prev.next = self.next
         del self.parent.D[self.value]
+        del self
 
 class LinkedList(object):
 
@@ -68,13 +69,15 @@ for num in cups_list:
 
 current_cup_node = cups_linked_list.find(cups_list[0])
 
+del cups_list
+
 for i in range(0, 10000000):
 
     current_cup = current_cup_node.value
     pick_up = []
     pick_up_node = current_cup_node.next
 
-    for j in range(3):
+    for j in range(0, 3):
         pick_up.append(pick_up_node.value)
         tmp = pick_up_node.next
         pick_up_node.erase()
@@ -98,6 +101,8 @@ for i in range(0, 10000000):
 
     for cup in pick_up:
         dest_node = dest_node.insert(cup)
+
+    del dest_node
 
     current_cup_node = cups_linked_list.find(current_cup)
     current_cup_node = current_cup_node.next
