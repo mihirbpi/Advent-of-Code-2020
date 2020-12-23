b81723bdf0d15ebc58ccba11dde20e1b27b2bef4
mihirbpi <mihir.borkar@gmail.com>
2020-12-23 14:15:43 -0800

    Some small improvements to Part 2

--- adv22/adv22-2.py
+++ adv22/adv22-2.py
@@ -1,5 +1,4 @@
 from collections import deque
-import copy
 from aocd import get_data
 
 my_list = get_data(day=22).split("\n\n")
@@ -8,19 +7,13 @@ p2_string = list(map(int, my_list[1].strip("\n").split("\n")[1:]))
 
 def game(p1_deck_list, p2_deck_list):
     past_configs_list = []
-    p1_deck = deque()
-    p2_deck = deque()
+    p1_deck = deque(p1_deck_list)
+    p2_deck = deque(p2_deck_list)
 
-    for i in range(0, len(p1_deck_list)):
-        p1_deck.append(p1_deck_list[i])
-
-    for i in range(0, len(p2_deck_list)):
-        p2_deck.append(p2_deck_list[i])
-
-    while(not len(p1_deck) == 0 and not len(p2_deck) == 0):
+    while(len(p1_deck) > 0 and len(p2_deck) > 0):
 
         if([list(p1_deck), list(p2_deck)] in past_configs_list):
-            return (0, 1)
+            return (p1_deck, 1)
 
         past_configs_list.append([list(p1_deck), list(p2_deck)])
         p1_card = p1_deck.popleft()
@@ -33,7 +26,7 @@ def game(p1_deck_list, p2_deck_list):
                 p1_deck.append(p1_card)
                 p1_deck.append(p2_card)
 
-            elif(winner == 2):
+            else:
                 p2_deck.append(p2_card)
                 p2_deck.append(p1_card)
 
@@ -43,30 +36,20 @@ def game(p1_deck_list, p2_deck_list):
                 p1_deck.append(p1_card)
                 p1_deck.append(p2_card)
 
-            elif(p2_card > p1_card):
+            else:
                 p2_deck.append(p2_card)
                 p2_deck.append(p1_card)
 
-    winning_cards = []
+    if(len(p1_deck) > 0):
+        return (p1_deck, 1)
 
-    if(not len(p1_deck) == 0):
+    else:
+        return (p2_deck, 2)
 
-        winning_cards = list(p1_deck)
-        winning_score = 0
+winning_deck = list(game(p1_string, p2_string)[0])[::-1]
+winning_score = 0
 
-        for i in range(0,len(winning_cards[::-1])):
-            winning_score += (i + 1) * winning_cards[::-1][i]
+for i in range(0, len(winning_deck)):
+    winning_score += (i + 1) * winning_deck[i]
 
-        return (winning_score, 1)
-
-    elif(not len(p2_deck) == 0):
-
-        winning_cards = list(p2_deck)
-        winning_score = 0
-
-        for i in range(0,len(winning_cards[::-1])):
-            winning_score += (i + 1) * winning_cards[::-1][i]
-
-        return (winning_score, 2)
-
-print(game(p1_string, p2_string)[0])
+print(winning_score)
