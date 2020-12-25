bdf5b9c321e2ac4b24d8034bdb54287ba5289cef
mihirbpi <mihir.borkar@gmail.com>
2020-12-25 10:12:25 -0800

    Solved Day 25 Part 1. There is no Part 2

---
+++ adv25/adv25-1.py
@@ -0,0 +1,33 @@
+from aocd import get_data
+import math
+
+my_list = get_data(day=25).split("\n")
+door_public_key = int(my_list[0])
+card_public_key = int(my_list[1])
+
+def discrete_log(public_key):
+    i = 0
+    previous = 0
+    b = 20201227
+
+    while(True):
+
+        a = pow(7, i, 20201227)
+
+        if (a == public_key):
+            return i
+
+        b = b - a + previous
+
+        if(b == public_key):
+            return i + int((20201227 - 1) / 2)
+
+        i = i + 1
+        previous = a
+
+def transform(subject_number, loop_size):
+    return pow(subject_number, loop_size, 20201227)
+
+door_loop_size = discrete_log(door_public_key)
+encryption_key = transform(card_public_key, door_loop_size)
+print(encryption_key)
