a1a7b1638b0e761f1638fa48af01a7d63fc18f26
mihirbpi <mihir.borkar@gmail.com>
2020-12-24 16:42:42 -0800

    Rewrite of Day 24 Part 2

--- adv24/adv24-2.py
+++ adv24/adv24-2.py
@@ -4,6 +4,8 @@ import copy
 
 my_list = get_data(day=24).split("\n")
 
+tile_color_dict = defaultdict(lambda: [1, 0])
+
 def instruction_to_tile(instruction):
     current_tile = [0, 0]
     i = 0
@@ -40,41 +42,80 @@ def instruction_to_tile(instruction):
 
     return tuple(current_tile)
 
-def update(black_tiles):
+def neighbors_count(tile_color_dict, tile):
 
-    new_black_tiles = set()
-    neighbors_dict = defaultdict(lambda: 0)
+    count = 0
 
-    for tile in black_tiles:
+    for delta in [[1, -3], [-1, -3], [1, 3], [-1, 3], [2, 0], [-2, 0]]:
+
+        if(tile_color_dict[(tile[0] + delta[0], tile[1] + delta[1])][0] == -1):
+            count += 1
+
+    return count
+
+def update_neighbors(tile_color_dict):
+
+    new_tile_color_dict = copy.deepcopy(tile_color_dict)
+    tiles = copy.deepcopy(list(tile_color_dict.keys()))
+
+    for tile in tiles:
+
+        new_tile_color_dict[tile][1] = neighbors_count(tile_color_dict, tile)
+
+    return new_tile_color_dict
+
+def update(tile_color_dict):
+
+    new_tile_color_dict = copy.deepcopy(tile_color_dict)
+    tiles = copy.deepcopy(list(tile_color_dict.keys()))
+
+    for tile in tiles:
+
+        if(tile_color_dict[tile][0] == -1):
+
+            if(neighbors_count(tile_color_dict, tile) == 0 or neighbors_count(tile_color_dict, tile) > 2):
+
+                if(tile in new_tile_color_dict):
+                    new_tile_color_dict.pop(tile)
+
+        elif(tile_color_dict[tile][0] == 1):
+
+            if(neighbors_count(tile_color_dict, tile) == 2):
+                new_tile_color_dict[tile][0] = -1
+                new_tile_color_dict[tile][1] = 2
 
         for delta in [[1, -3], [-1, -3], [1, 3], [-1, 3], [2, 0], [-2, 0]]:
-            neighbors_dict[(tile[0] + delta[0], tile[1] + delta[1])] += 1
+            adjacent_tile = (tile[0] + delta[0], tile[1] + delta[1])
 
-    for tile in neighbors_dict:
+            if(tile_color_dict[adjacent_tile][0] == -1):
 
-        if(tile in black_tiles and neighbors_dict[tile] <= 2):
-           new_black_tiles.add(tile)
+                if(neighbors_count(tile_color_dict, adjacent_tile) == 0 or neighbors_count(tile_color_dict, adjacent_tile) > 2):
 
-        if(tile not in black_tiles and neighbors_dict[tile] == 2):
-            new_black_tiles.add(tile)
+                    if(adjacent_tile in new_tile_color_dict):
+                        new_tile_color_dict.pop(adjacent_tile)
 
-    return new_black_tiles
+            elif(tile_color_dict[adjacent_tile][0] == 1):
 
-black_tiles = set()
+                if(neighbors_count(tile_color_dict, adjacent_tile) == 2):
+                    new_tile_color_dict[adjacent_tile][0] = -1
+                    new_tile_color_dict[adjacent_tile][1] = 2
+
+    return new_tile_color_dict
 
 for i in range(0, len(my_list)):
-
     instruction = my_list[i]
     tile = instruction_to_tile(instruction)
-
-    if(tile in black_tiles):
-        black_tiles.remove(tile)
-
-    else:
-        black_tiles.add(tile)
+    tile_color_dict[tile][0] *= -1
 
 for day in range(0, 100):
+    tile_color_dict = update_neighbors(tile_color_dict)
+    tile_color_dict = update(tile_color_dict)
 
-    black_tiles = update(black_tiles)
+black_tile_count = 0
 
-print(len(black_tiles))
+for tile in tile_color_dict:
+
+    if(tile_color_dict[tile][0] == -1):
+        black_tile_count += 1
+
+print(black_tile_count)
